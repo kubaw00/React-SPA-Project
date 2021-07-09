84f13c904f20842de3c3af3e7e61f89573cd0a9b
Jakub Widurek <jakubwidurek@gmail.com>
2021-07-09 14:58:37 +0200

    Updates

---
+++ precache-manifest.f92e17ed37972c139ba5655a4343795b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9adfcd293807b7f2f4ea",
+    "url": "/React-SPA-Project/static/css/main.1b1f149e.chunk.css"
+  },
+  {
+    "revision": "9adfcd293807b7f2f4ea",
+    "url": "/React-SPA-Project/static/js/main.9adfcd29.chunk.js"
+  },
+  {
+    "revision": "6ca6fdfddc33b8ab7f19",
+    "url": "/React-SPA-Project/static/js/runtime~main.6ca6fdfd.js"
+  },
+  {
+    "revision": "5a9d91031e4420e1edd0",
+    "url": "/React-SPA-Project/static/js/2.5a9d9103.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/React-SPA-Project/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/React-SPA-Project/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/React-SPA-Project/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "824bc4de070cc8748bcc949e2bd32e0c",
+    "url": "/React-SPA-Project/index.html"
+  }
+];
